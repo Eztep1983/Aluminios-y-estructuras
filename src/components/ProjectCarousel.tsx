25a5be1a3edfe81f89b70675d92174ac1b7ad190
Esteban Alexander Zambrano <extep1983@gmail.com>
2025-11-18 21:39:29 +0000

    Make the carrousel to auto slide

--- src/components/ProjectCarousel.tsx
+++ src/components/ProjectCarousel.tsx
@@ -9,18 +9,27 @@ import {
 } from '@/components/ui/carousel';
 import type { Project } from '@/lib/types';
 import { ProjectCard } from './ProjectCard';
+import Autoplay from "embla-carousel-autoplay";
+import { useRef } from 'react';
 
 interface ProjectCarouselProps {
   projects: Project[];
 }
 
 export function ProjectCarousel({ projects }: ProjectCarouselProps) {
+  const plugin = useRef(
+    Autoplay({ delay: 3000, stopOnInteraction: true })
+  );
+
   return (
     <Carousel
       opts={{
         align: 'start',
         loop: true,
       }}
+      plugins={[plugin.current]}
+      onMouseEnter={plugin.current.stop}
+      onMouseLeave={plugin.current.reset}
       className="w-full"
     >
       <CarouselContent>
